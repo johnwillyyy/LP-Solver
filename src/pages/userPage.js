4c723e04cc827a0cf67d1a954ac061f7a534957f
John William <johnwilliamfawzy@gmail.com>
2025-03-24 18:36:25 +0200

    set animation time

--- src/pages/userPage.js
+++ src/pages/userPage.js
@@ -121,7 +121,7 @@ const LinearProgrammingSolver = () => {
             problemType
           }
         });
-      }, 1500);
+      }, 2000);
     } catch (error) {
       console.error("Error sending data:", error);
     }
