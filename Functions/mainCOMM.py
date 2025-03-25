7ff903ecb9573b3e7ca2f92bbad2e97d60d11fc6
John William <johnwilliamfawzy@gmail.com>
2025-03-25 02:53:40 +0200

    weighted goals handles in UI

--- Functions/mainCOMM.py
+++ Functions/mainCOMM.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def main(received_data):
 
-    problem_type, objective_type, technique, objective_coefficients, constraint_coefficients, constraint_operators, constraint_rhs, goal_coefficients, goal_operators, goal_rhs, goal_priorities, unrestricted_variables = process_lp_data(received_data)
+    problem_type, objective_type, technique, objective_coefficients, constraint_coefficients, constraint_operators, constraint_rhs, goal_coefficients, goal_operators, goal_rhs, goal_type,goal_priorities,goal_weights, unrestricted_variables = process_lp_data(received_data)
 
     is_goal = False
     if problem_type == 'goal':
@@ -14,7 +14,10 @@ def main(received_data):
 
     solver = LinearProgrammingSolver(c=objective_coefficients, A=constraint_coefficients, b=constraint_rhs, constraint_types=constraint_operators, method=technique, objective=objective_type,unrestricted_vars=unrestricted_variables, is_goal=is_goal,
                                      goal_coeffs=goal_coefficients, goal_rhs=goal_rhs,goal_signs= goal_operators,priorities=goal_priorities)
-    
+    print('zzzzz')
+    print(goal_type)
+    print(goal_weights)
+    print(goal_priorities)
     if is_goal:
         goal_status, solution,tableaux = solver.solve()
         return "", goal_status, solution, tableaux
@@ -24,7 +27,6 @@ def main(received_data):
         # print("\nStatus:")
         # print("\nOptimal Solution:", solution)
         # print("Optimal Value:", optimal_value)
-        solver.print_tableau_steps()
         return  status,solution,optimal_value, tableaux
 
 
